b4a9620bd8f45eb0236fd897d07d9ec3d0da0916
Luc Mahoux <luc.mahoux@fuego-app.com>
2024-04-17 20:31:13 +0200

    feat: messy code to test linter

---
+++ TestProject/TestProject/TestClass.cs
@@ -0,0 +1,31 @@
+public class TestClass
+{
+    private int _unusedVariable;
+
+    public void TestMethod()
+    {
+        int x;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                if (i == j)
+                {
+                    Console.WriteLine("Match found");
+                }
+            }
+        }
+    }
+
+    public void AnotherTestMethod()
+    {
+        try
+        {
+            // Empty try block
+        }
+        catch
+        {
+            // Empty catch block
+        }
+    }
+}
